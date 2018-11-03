b4bd79003bd3e9ffcf6c8b79f878d3bd41f98d30
Marcin Ciesinski <casin1903@gmail.com>
2018-11-03 10:50:45 +0100

    cwiczenia z c

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+void wczytajDane(int* a, int* b);
+void wypiszWyniki(int k, int m);
+
 int main()
 {
-    printf("hello world\n");
+  int x, y;
+  wczytajDane(&x, &y);
+  wypiszWyniki(x, y);
+}
+
+void wczytajDane(int* a, int* b){
+  printf("podaj a, b: ");
+  scanf("%d %d", a, b);
+}
+
+void wypiszWyniki(int k, int m){
+  printf ("Wyniki czterech działań na x = %d, y = %d\n", k, m);
+  printf ("x + y = %d\n", k+m);
 }
