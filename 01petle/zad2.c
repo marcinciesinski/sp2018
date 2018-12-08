169d907b2be0ea1e547c925daf511ee14bf41e2f
Marcin Ciesinski <casin1903@gmail.com>
2018-12-08 09:32:04 +0100

    Extract function in zad2

--- 01petle/zad2.c
+++ 01petle/zad2.c
@@ -11,11 +11,28 @@
 #include <stdio.h>
 
 void chestPrinter();
+void printIndent(int i);
+void rowPrinter(int row);
 
 int main()
 {
   printf("Program drukuje szachownicę:");
   chestPrinter();
+}
+
+void printIndent(int i)
+{
+  if (i % 2 == 1)
+    printf(" ");
+}
+
+void rowPrinter(int row)
+{
+  for (int i = 0; i < row; i++)
+    {
+      printf("*");
+      printf(" ");
+    }
 }
 
 void chestPrinter()
@@ -25,13 +42,8 @@ void chestPrinter()
   for (int i = 0; i < col; i++)
   {
     printf("\n");
-    if (i % 2 == 1)
-      printf(" ");
-    for (int i = 0; i < row; i++)
-    {
-      printf("*");
-      printf(" ");
-    }
+    printIndent(i);
+    rowPrinter(row);
   }
   printf("\n");
 }
